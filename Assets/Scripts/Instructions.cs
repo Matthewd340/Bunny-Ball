911777db1e94005c8934ed40347e4da5497d55bc
Matthewd340 <matthewd340@nycstudents.net>
2024-05-13 09:19:59 -0400

    Added game instructions

---
+++ Assets/Scripts/Instructions.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instructions : MonoBehaviour
+{
+    private string message = "Move with W. A, S. D. FInd the keys in each maze.";
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(message);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
